de4fa60a9112a3a15d9121f3a58b2d2f783c222c
kimjunseoo <zz0905k@naver.com>
2023-04-10 18:45:02 +0900

    20230410 Can't Solving baekjoon 1629

--- source/baekjoon/b1629.cpp
+++ source/baekjoon/b1629.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+typedef long long ll;
+ll A, B, C;
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
 
-    
+    cin >> A >> B >> C;
+
+
     return 0;
 }
