0d0c529bc71694b8ebac35bc92e862d575d013e1
kimjunseoo <zz0905k@naver.com>
2023-04-10 17:36:10 +0900

    20230410 Solving baekjoon 1940

--- a.cpp
+++ a.cpp
@@ -1,27 +1,8 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-string str;
 
 int main(){
-
-    getline(cin, str);
-
-    for (int i = 0; i < str.size(); i++)
-    {   
-        if(isalpha(str[i]) == 2 && (int)str[i] >= 78){
-            str[i] -= 13;
-        } else if(isalpha(str[i]) == 2){
-            str[i] += 13;
-        } else if(isalpha(str[i]) == 1 && (int)str[i] >= 110){
-            str[i] -= 13;
-        } else if(isalpha(str[i]) == 1){
-            str[i] += 13;
-        }
-    }
-    
-
-    cout << str << '\n';
 
     return 0;
 }
